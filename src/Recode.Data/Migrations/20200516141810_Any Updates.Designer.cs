2763c41afb04fa775cda98864fe25901149d4e1f
Samuel Awonorin <samuel.awonorin@venturegardengroup.com>
2020-05-16 15:19:50 +0100

    Added missing migration for roles table

--- src/Recode.Data/Migrations/20200516141810_Any Updates.Designer.cs
+++ src/Recode.Data/Migrations/20200516141810_Any Updates.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Recode.Data;
 
 namespace Recode.Data.Migrations
 {
     [DbContext(typeof(APPContext))]
-    partial class APPContextModelSnapshot : ModelSnapshot
+    [Migration("20200516141810_Any Updates")]
+    partial class AnyUpdates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Recode.Data/Migrations/20200516141810_Any Updates.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Recode.Data.Migrations
+{
+    public partial class AnyUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "DateCreated",
+                value: new DateTimeOffset(new DateTime(2020, 5, 16, 15, 18, 10, 316, DateTimeKind.Unspecified).AddTicks(4688), new TimeSpan(0, 1, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "DateCreated",
+                value: new DateTimeOffset(new DateTime(2020, 5, 16, 15, 18, 10, 316, DateTimeKind.Unspecified).AddTicks(6913), new TimeSpan(0, 1, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "DateCreated",
+                value: new DateTimeOffset(new DateTime(2020, 5, 16, 15, 18, 10, 316, DateTimeKind.Unspecified).AddTicks(6931), new TimeSpan(0, 1, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "DateCreated",
+                value: new DateTimeOffset(new DateTime(2020, 5, 16, 15, 18, 10, 316, DateTimeKind.Unspecified).AddTicks(6931), new TimeSpan(0, 1, 0, 0, 0)));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "DateCreated",
+                value: new DateTimeOffset(new DateTime(2020, 5, 16, 14, 53, 23, 896, DateTimeKind.Unspecified).AddTicks(5684), new TimeSpan(0, 1, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "DateCreated",
+                value: new DateTimeOffset(new DateTime(2020, 5, 16, 14, 53, 23, 896, DateTimeKind.Unspecified).AddTicks(7955), new TimeSpan(0, 1, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "DateCreated",
+                value: new DateTimeOffset(new DateTime(2020, 5, 16, 14, 53, 23, 896, DateTimeKind.Unspecified).AddTicks(7972), new TimeSpan(0, 1, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "DateCreated",
+                value: new DateTimeOffset(new DateTime(2020, 5, 16, 14, 53, 23, 896, DateTimeKind.Unspecified).AddTicks(7976), new TimeSpan(0, 1, 0, 0, 0)));
+        }
+    }
+}
